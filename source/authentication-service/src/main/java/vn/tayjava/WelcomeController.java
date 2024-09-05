f0253782992ab7ebaa54e86373fe2c4ffa19d77e
Tây Java <quoctay87@gmail.com>
2024-09-05 21:04:05 +0700

    Update readme

--- source/authentication-service/src/main/java/vn/tayjava/WelcomeController.java
+++ source/authentication-service/src/main/java/vn/tayjava/WelcomeController.java
@@ -1,22 +1,24 @@
 package vn.tayjava;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RefreshScope
 @RestController
 public class WelcomeController {
 
-    private RemoteService remoteService;
+    private final RestTemplate restTemplate;
+
+    public WelcomeController(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
 
     @Value("${message.welcome:Hello!}")
     private String message;
-
-    public WelcomeController(RemoteService remoteService) {
-        this.remoteService = remoteService;
-    }
 
     @GetMapping("/welcome")
     public String welcome() {
@@ -24,9 +26,20 @@ public class WelcomeController {
         return message;
     }
 
+    @CircuitBreaker(name = "accountServiceCircuitBreaker", fallbackMethod = "errorMessage")
     @GetMapping("/call-account-service")
     public String callAccountService() {
-        return remoteService.callAccountService();
+        String response = restTemplate.getForObject("http://localhost:4953/account/service-name", String.class);
+        return "<h1>Hello from the " + response + "!</h1>";
+    }
+
+    /**
+     * Fall back method
+     * @param throwable
+     * @return string
+     */
+    public String errorMessage(Throwable throwable) {
+        return "Service unavailable, please try again!";
     }
 
 }
